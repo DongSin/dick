41845284261c800f9a6b82c3bdd197dd4873fb3b
DongSin <43740391+DongSin@users.noreply.github.com>
2019-01-06 17:01:31 +0800

    Add files via upload

--- MemberDA.java
+++ MemberDA.java
@@ -1,3 +1,4 @@
+
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
@@ -51,7 +52,28 @@ public class MemberDA {
         if (rs.next()) {
             String name = rs.getString("member_name");
             String password = rs.getString("member_password");
-            String email = rs.getString("member_email");
+            String email = rs.getString("email");
+            Book[] cart = getCart(id);
+            Purchase[] purchase = getPurchase(id);
+            member = new Member(id, name, password, email, cart, purchase);
+        }
+
+        return member;
+    }
+
+
+    public static Member searchMemberByName(String name) throws SQLException, IOException {
+        closeConnection();
+        createConnection();
+        Member member = null;
+        sql = "select * from member where member_name = ?";
+        stmt = conn.prepareStatement(sql);
+        stmt.setString(1, name);     
+        rs = stmt.executeQuery();
+        if (rs.next()) {
+            String id = rs.getString("member_id");
+            String password = rs.getString("member_password");
+            String email = rs.getString("email");
             Book[] cart = getCart(id);
             Purchase[] purchase = getPurchase(id);
             member = new Member(id, name, password, email, cart, purchase);
@@ -71,82 +93,18 @@ public class MemberDA {
         stmt.setString(4, member.getEmail());
         stmt.executeUpdate();
         closeConnection();
-        insertCart(member.getMemberId(), member.getMemberCart());
-        insertPurchaseHistory(member.getMemberId(), member.getPurchaseHistory());
-
     }
 
     public static void updateMember(Member member) throws SQLException {
         closeConnection();
         createConnection();
-        sql = "update member set member_name = ?, member_password = ?, member_email = ? where member_id = ?";
+        sql = "update member set member_name = ?, member_password = ?, email = ? where member_id = ?";
         stmt = conn.prepareStatement(sql);
         stmt.setString(1, member.getMemberName());
         stmt.setString(2, member.getPassword());
         stmt.setString(3, member.getEmail());
         stmt.setString(4, member.getMemberId());
         stmt.executeUpdate();
-        closeConnection();
-
-        clearCart(member.getMemberId());
-        insertCart(member.getMemberId(), member.getMemberCart());
-        clearPurchaseHistory(member.getMemberId());
-        insertPurchaseHistory(member.getMemberId(), member.getPurchaseHistory());
-        
-    }
-
-    private static void clearCart(String memberId) throws SQLException {
-        closeConnection();
-        createConnection();
-        sql = "delete from cart where member_id = ?";
-        stmt = conn.prepareStatement(sql);
-        stmt.setString(1, memberId);
-        stmt.executeUpdate();
-        closeConnection();
-    }
-
-    private static void insertCart(String memberId, Book[] cart) throws SQLException {
-        closeConnection();
-        createConnection();
-        sql = "insert into cart values(?,?)";
-        stmt = conn.prepareStatement(sql);
-        for (int i = 0; i < cart.length; i++) {
-            Book book = cart[i];
-            if (book != null) {
-                stmt.setString(1, memberId);
-                stmt.setString(2, book.getBookId());
-                stmt.executeUpdate();
-            }
-        }
-        closeConnection();
-    }
-
-    private static void clearPurchaseHistory(String memberId) throws SQLException {
-        closeConnection();
-        createConnection();
-        sql = "delete from purchase where member_id = ?";
-        stmt = conn.prepareStatement(sql);
-        stmt.setString(1, memberId);
-        stmt.executeUpdate();
-        closeConnection();
-    }
-
-    private static void insertPurchaseHistory(String memberId, Purchase[] p) throws SQLException {
-        closeConnection();
-        createConnection();
-        sql = "insert into purchase values(?,?,?,?)";
-        stmt = conn.prepareStatement(sql);
-        int i = 0;
-        while (i < p.length && p[i] != null) {
-            stmt.setString(1, memberId);
-            stmt.setString(2, p[i].getPurchaseBooks().getBookId());
-            Date date = new Date(p[i].getPurchaseDay(), p[i].getPurchaseMonth(), p[i].getPurchaseYear());
-            stmt.setDate(3, date);
-            stmt.setInt(4, p[i].getQuantity());
-            stmt.executeUpdate();
-            i++;
-        }
-        
         closeConnection();
     }
 
@@ -207,7 +165,7 @@ public class MemberDA {
         return purchases;
     }
 
-//    private static Book[] getPurchaseBooks(String id, Date date) throws SQLException, IOException {
+//    private static Book[] getPurchaseBooks(String id, Date date) throws SQLException {
 //        closeConnection();
 //        createConnection();
 //        sql = "select book_id from purchase where member_id = ? and purchase_date = ?";
@@ -254,6 +212,81 @@ public class MemberDA {
             conn.close();
         }
 
+    }
+    
+    private static void clearCart(String memberId) throws SQLException {
+        closeConnection();
+        createConnection();
+        sql = "delete from cart where member_id = ?";
+        stmt = conn.prepareStatement(sql);
+        stmt.setString(1, memberId);
+        stmt.executeUpdate();
+        closeConnection();
+    }
+
+    private static void insertCart(String memberId, Book[] cart) throws SQLException {
+        closeConnection();
+        createConnection();
+        sql = "insert into cart values(?,?)";
+        stmt = conn.prepareStatement(sql);
+        for (int i = 0; i < cart.length; i++) {
+            Book book = cart[i];
+            if (book != null) {
+                stmt.setString(1, memberId);
+                stmt.setString(2, book.getBookId());
+                stmt.executeUpdate();
+            }
+        }
+        closeConnection();
+    }
+
+    private static void clearPurchaseHistory(String memberId) throws SQLException {
+        closeConnection();
+        createConnection();
+        sql = "delete from purchase where member_id = ?";
+        stmt = conn.prepareStatement(sql);
+        stmt.setString(1, memberId);
+        stmt.executeUpdate();
+        closeConnection();
+    }
+
+    private static void insertPurchaseHistory(String memberId, Purchase[] p) throws SQLException {
+        closeConnection();
+        createConnection();
+        sql = "insert into purchase values(?,?,?,?)";
+        stmt = conn.prepareStatement(sql);
+        int i = 0;
+        while (i < p.length && p[i] != null) {
+            stmt.setString(1, memberId);
+            stmt.setString(2, p[i].getPurchaseBooks().getBookId());
+            Date date = new Date(p[i].getPurchaseDay(), p[i].getPurchaseMonth(), p[i].getPurchaseYear());
+            stmt.setDate(3, date);
+            stmt.setInt(4, p[i].getQuantity());
+            stmt.executeUpdate();
+            i++;
+        }
+        
+        closeConnection();
+    }
+    
+    public static int getMemberCount() throws SQLException{
+        closeConnection();
+        createConnection();
+        int count;
+        String lastId;
+        sql = "select member_id from member order by member_id desc FETCH FIRST 1 ROWS ONLY";
+        stmt = conn.prepareStatement(sql);
+        rs = stmt.executeQuery();
+        if(rs.next()){
+            lastId = rs.getString(1);
+            lastId = lastId.replace('M', '0');
+            count = Integer.parseInt(lastId);
+        }else{
+            count = 0;
+        }
+        closeConnection();
+        return count;
+        
     }
 
 }
